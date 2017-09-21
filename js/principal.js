25d3706a1a2529ac290f443cc98b8ab3a34b22a2
Mário dos Santos <mariotascorporation@gmail.com>
2017-09-21 11:04:33 -0300

    Inclusão e tratamento de mensagem do HLCLOUD

--- js/principal.js
+++ js/principal.js
@@ -1,3 +1,17 @@
+$(function() {
+	$("div.alert-success").on('click', function() {
+		var newMessage = $("p.alert-warning").text();
+		if (newMessage.length > 0) {
+			newMessage = newMessage.trim();
+			var stringStart = newMessage.indexOf(" : ");
+			var stringStop = newMessage.indexOf("]", stringStart);
+			if (stringStart > 0 && stringStop > 0) {
+				$("div.alert-success").html(newMessage.substring(stringStart + 2, stringStop + 1));
+			}
+		}
+	});
+});
+
 var banners = ["Do lixo ao luxo!", "Reaproveitar é aproveitar!"];
 var bannerAtual = 0;
 
@@ -7,3 +21,4 @@ function trocaBanner() {
 }
 
 setInterval(trocaBanner, 1000);
+
